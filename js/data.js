6d9e5f063af7a6bca0be0a56e07b21a5c8af75c3
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-24 11:50:09 +0300

    add

--- js/data.js
+++ js/data.js
@@ -7,13 +7,14 @@ var cartoons = [
     "image": "https://zbk.ru/wp-content/uploads/2020/02/nu-pogodi-souyzmultfilm.jpg"
     },
     {
-    "id": 1,
+    "id": 2,
     "title": "Tom and Jerry",
     "year": 1940,
     "summary": "Cерия короткометражных мультфильмов, созданная американскими аниматорами Уильямом Ханной и Джозефом Барберой. В сюжетной линии каждой части изображено (с элементами лёгкой житейской сатиры) соперничество главных героев — кота Тома и мышонка Джерри[1]. Пилотный выпуск проекта «Кот получает пинка» вышел на экраны 20 марта 1940 года",
     "image": "https://images-na.ssl-images-amazon.com/images/I/8134XeLmkRL._RI_.jpg"
     },
     {
+    "id": 3,    
     "title": "The Smurfs",
     "year": 1958,
     "summary": "The Smurfs (French: Les Schtroumpfs; Dutch: De Smurfen) is a Belgian comic franchise centered on a fictional colony of small, blue, humanoid creatures who live in mushroom-shaped houses in the forest. The Smurfs was first created and introduced as a series of comic characters by the Belgian comics artist Peyo (the pen name of Pierre Culliford) in 1958, wherein they were known as Les Schtroumpfs.",
