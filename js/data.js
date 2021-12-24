2cdc1de13a8d3636ad6acf39492f2b8f20328e7f
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-24 11:14:20 +0300

    add

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var movies = [{
+    "title": "Patton Oswalt: Annihilation",
+    "year": 2017,
+    "categories": [
+        "Uncategorized"
+    ],
+    "imdbId": "tt7026230",
+    "imdbRating": 7.4,
+    "runtime": 66,
+    "language": "English",
+    "youtubeId": "4hZi5QaMBFc",
+    "summary": "Patton Oswald, despite a personal tragedy, produces his best standup yet. Focusing on the tribulations of the Trump era and life after the loss of a loved one, Patton Oswald continues his journey to contribute joy to the world.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/maxresdefault.jpg"
+}]
+
+var cartoons = [
+    {
+    "title": "Ну, погоди!",
+    "year": 1969,
+    "summary": "«Ну, погоди́!» — популярная серия советских и российских мультфильмов. Пилотный выпуск был снят режиссёром Геннадием Сокольским, и вышел на экраны в 1969 г., в том же году вышел первый мультфильм основной серии. В основе сюжета лежит постоянно оканчивающаяся неудачей погоня Волка за Зайцем, после которой Заяц всегда оказывается невредим, а Волк говорит или кричит ему: «Ну, Заяц, ну, погоди!».",
+    "image": "https://zbk.ru/wp-content/uploads/2020/02/nu-pogodi-souyzmultfilm.jpg"
+    },
+    {
+    "title": "Tom and Jerry",
+    "year": 1940,
+    "summary": "Cерия короткометражных мультфильмов, созданная американскими аниматорами Уильямом Ханной и Джозефом Барберой. В сюжетной линии каждой части изображено (с элементами лёгкой житейской сатиры) соперничество главных героев — кота Тома и мышонка Джерри[1]. Пилотный выпуск проекта «Кот получает пинка» вышел на экраны 20 марта 1940 года",
+    "image": "https://images-na.ssl-images-amazon.com/images/I/8134XeLmkRL._RI_.jpg"
+    },
+    {
+    "title": "The Smurfs",
+    "year": 1958,
+    "summary": "The Smurfs (French: Les Schtroumpfs; Dutch: De Smurfen) is a Belgian comic franchise centered on a fictional colony of small, blue, humanoid creatures who live in mushroom-shaped houses in the forest. The Smurfs was first created and introduced as a series of comic characters by the Belgian comics artist Peyo (the pen name of Pierre Culliford) in 1958, wherein they were known as Les Schtroumpfs.",
+    "image": "https://zbk.ru/wp-content/uploads/2020/02/nu-pogodi-souyzmultfilm.jpg"
+    },
+]
+
