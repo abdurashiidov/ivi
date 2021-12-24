f98c78b95a0a007b3e7a08e54326594cdf6c7bde
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-24 11:58:16 +0300

    add

--- js/data.js
+++ js/data.js
@@ -53,7 +53,7 @@ var cartoons = [
     "title": "",
     "year": "",
     "summary": "",
-    "image": ""
+    "image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png"
     },
    
 ]
